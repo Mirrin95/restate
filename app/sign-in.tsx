a0b94a60ee88f8353ebb505cd2a4a8e19ae48d34
Mike Rincon <michaelandresrincongarcia@gmail.com>
2024-12-25 19:49:18 -0500

    sc2512204

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,28 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
 
 const signin = () => {
   return (
-    <View>
-      <Text>signin</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+        <View className="flex items-center px-10">
+          <Text className="text-3xl font-bold my-10 font-rubik text-black-200">
+            Welcome to ReState!
+          </Text>
+          <Text className="text-3xl  mt-2 font-rubik-bold text-black-300 text-center">
+            Lets Get You Closer to {"\n"}{" "}
+            <Text className="text-primary-300">Your Dream Home</Text>
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
